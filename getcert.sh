69efed1afa3a03e5a12a32ab279c64adfc53b232
Ayahito Saji <ayahito.saji@gmail.com>
2023-10-12 04:45:37 +0900

    コマンドの修正

--- getcert.sh
+++ getcert.sh
@@ -19,5 +19,43 @@ echo "Successfully started nginx"
 
 echo "Staring certbot..."
 docker compose run certbot certonly --webroot -d ${domain} -w /var/www/certbot --agree-tos --no-eff-email
+echo "Successfully generated certificate"
 
 docker compose down nginx > /dev/null 2>&1
+
+read -p "Please enter your app port (3000): " port
+
+cat <<EOL > nginx.conf
+server {
+    listen      80;
+    server_name yourdomain.com;
+    location ~ /.well-known/acme-challenge {
+        root /var/www/certbot;
+        allow all;
+    }
+    location / {
+        return 301 https://\$host\$request_uri;
+    }
+}
+
+server {
+    listen      443 ssl;
+    server_name ${domain};
+
+    ssl_certificate /etc/letsencrypt/live/${domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/${domain}/privkey.pem;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_prefer_server_ciphers off;
+
+    location / {
+        proxy_pass http://172.17.0.1:${port};
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOL
+
+echo "Successfully generated nginx.conf"
+echo "Run docker compose to start nginx and certbot to renew certificates"
+echo "$ docker compose up -d"
