f1a5b09702f48057a521d6224eab080da9efe945
Ayahito Saji <ayahito.saji@gmail.com>
2023-10-12 03:17:24 +0900

    getcertの作成

---
+++ README.md
@@ -0,0 +1,11 @@
+# httpsify
+
+1.  ソースコードのダウンロード
+    ```
+    $ git clone git@github.com:ayahito-saji/httpsify.git
+    ```
+
+1.  コマンドを実行
+    ```
+    $ bash ./getcert.sh
+    ```
---
+++ getcert.sh
@@ -0,0 +1,21 @@
+read -p "Please enter your domain (example.com): " domain
+
+cat <<EOL > nginx.conf
+server {
+    listen 80;
+    server_name ${domain};
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+        try_files \$uri = 404;
+    }
+}
+EOL
+
+docker-compose down
+echo "Starting nginx..."
+docker-compose up -d nginx
+echo "Successfully started nginx"
+
+echo "Staring certbot..."
+docker-compose run certbot certonly --webroot -w /var/www/certbot -d ${domain}
